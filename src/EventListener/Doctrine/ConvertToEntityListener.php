513166b7f323d49d3f6444cd15534320f52a4199
Joachim Løvgaard <joachim@loevgaard.dk>
2024-03-12 13:30:01 +0100

    Check for interface instead

--- src/EventListener/Doctrine/ConvertToEntityListener.php
+++ src/EventListener/Doctrine/ConvertToEntityListener.php
@@ -6,13 +6,13 @@ namespace Setono\ClientBundle\EventListener\Doctrine;
 
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\Persistence\Event\LoadClassMetadataEventArgs;
-use Setono\ClientBundle\Entity\Client;
+use Setono\ClientBundle\Entity\ClientInterface;
 
 final class ConvertToEntityListener
 {
     public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs): void
     {
-        if ($eventArgs->getClassMetadata()->getName() !== Client::class) {
+        if (!is_a($eventArgs->getClassMetadata()->getName(), ClientInterface::class, true)) {
             return;
         }
 
