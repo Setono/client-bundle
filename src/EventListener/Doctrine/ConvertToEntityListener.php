309aa44c987e25b831b142d28ae5f4503e5c5460
Joachim Løvgaard <joachim@loevgaard.dk>
2024-03-12 13:29:18 +0100

    Remember to check for correct class

--- src/EventListener/Doctrine/ConvertToEntityListener.php
+++ src/EventListener/Doctrine/ConvertToEntityListener.php
@@ -6,11 +6,16 @@ namespace Setono\ClientBundle\EventListener\Doctrine;
 
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\Persistence\Event\LoadClassMetadataEventArgs;
+use Setono\ClientBundle\Entity\Client;
 
 final class ConvertToEntityListener
 {
     public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs): void
     {
+        if ($eventArgs->getClassMetadata()->getName() !== Client::class) {
+            return;
+        }
+
         $metadata = $eventArgs->getClassMetadata();
         if (!$metadata instanceof ClassMetadata) {
             return;
