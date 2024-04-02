609fa5ee067cbf971157318bc3090df39f0bf3a5
Joachim Løvgaard <joachim@loevgaard.dk>
2024-04-02 14:17:15 +0200

    Add comment

--- src/EventListener/Doctrine/ConvertToEntityListener.php
+++ src/EventListener/Doctrine/ConvertToEntityListener.php
@@ -6,13 +6,17 @@ namespace Setono\ClientBundle\EventListener\Doctrine;
 
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\Persistence\Event\LoadClassMetadataEventArgs;
-use Setono\ClientBundle\Entity\MetadataInterface;
+use Setono\ClientBundle\Entity\Metadata;
 
+/**
+ * If the user of the plugin doesn't add their own Metadata,
+ * this listener will make sure that Doctrine sees our Metadata as an entity
+ */
 final class ConvertToEntityListener
 {
     public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs): void
     {
-        if (!is_a($eventArgs->getClassMetadata()->getName(), MetadataInterface::class, true)) {
+        if (!is_a($eventArgs->getClassMetadata()->getName(), Metadata::class, true)) {
             return;
         }
 
