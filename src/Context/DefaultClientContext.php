33e1c9edf319138addffcfcc0df597422205c0ff
Joachim Løvgaard <joachim@loevgaard.dk>
2024-04-09 13:53:55 +0200

    Make sure that we only save metadata if any changes has been made to the metadata object

--- src/Context/DefaultClientContext.php
+++ src/Context/DefaultClientContext.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace Setono\ClientBundle\Context;
 
 use Setono\Client\Client;
+use Setono\ClientBundle\Client\ChangeAwareMetadata;
 
 final class DefaultClientContext implements ClientContextInterface
 {
     public function getClient(): Client
     {
-        return new Client();
+        return new Client(null, new ChangeAwareMetadata());
     }
 }
--- src/MetadataProvider/EmptyMetadataProvider.php
+++ src/MetadataProvider/EmptyMetadataProvider.php
@@ -4,12 +4,11 @@ declare(strict_types=1);
 
 namespace Setono\ClientBundle\MetadataProvider;
 
-use Setono\Client\Metadata;
 use Setono\ClientBundle\Client\ChangeAwareMetadata;
 
 final class EmptyMetadataProvider implements MetadataProviderInterface
 {
-    public function getMetadata(string $clientId): Metadata
+    public function getMetadata(string $clientId): ChangeAwareMetadata
     {
         return new ChangeAwareMetadata();
     }
