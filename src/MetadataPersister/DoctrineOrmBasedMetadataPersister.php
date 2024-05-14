39de5f2025792bf79224dc12fa2b85dee9a96864
Joachim Løvgaard <joachim@loevgaard.dk>
2024-05-14 15:27:22 +0200

    Handle UniqueConstraintViolationException when saving client id

--- src/MetadataPersister/DoctrineOrmBasedMetadataPersister.php
+++ src/MetadataPersister/DoctrineOrmBasedMetadataPersister.php
@@ -4,16 +4,22 @@ declare(strict_types=1);
 
 namespace Setono\ClientBundle\MetadataPersister;
 
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Doctrine\Persistence\ManagerRegistry;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 use Setono\Client\Client;
 use Setono\ClientBundle\Client\ChangeAwareMetadata;
 use Setono\ClientBundle\Client\LazyChangeAwareMetadata;
 use Setono\ClientBundle\Entity\MetadataInterface as MetadataEntityInterface;
 use Setono\Doctrine\ORMTrait;
 
-final class DoctrineOrmBasedMetadataPersister implements MetadataPersisterInterface
+final class DoctrineOrmBasedMetadataPersister implements MetadataPersisterInterface, LoggerAwareInterface
 {
     use ORMTrait;
+
+    private LoggerInterface $logger;
 
     public function __construct(
         ManagerRegistry $managerRegistry,
@@ -22,6 +28,7 @@ final class DoctrineOrmBasedMetadataPersister implements MetadataPersisterInterf
         private readonly string $metadataEntityClass,
     ) {
         $this->managerRegistry = $managerRegistry;
+        $this->logger = new NullLogger();
     }
 
     public function persist(Client $client): void
@@ -49,6 +56,17 @@ final class DoctrineOrmBasedMetadataPersister implements MetadataPersisterInterf
 
         $entity->setMetadata($metadata->toArray());
 
-        $manager->flush();
+        try {
+            $manager->flush();
+        } catch (UniqueConstraintViolationException) {
+            // todo how to handle this?
+
+            $this->logger->error(sprintf('Unique constraint violation occurred while persisting metadata for client id %s', $client->id));
+        }
+    }
+
+    public function setLogger(LoggerInterface $logger): void
+    {
+        $this->logger = $logger;
     }
 }
